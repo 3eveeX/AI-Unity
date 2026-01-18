5d2f75dbfd25917a4fc21c8d1028584ca688c6ab
3eveeX <hatchjavan@gmail.com>
2026-01-18 08:48:34 -0700

    Added the wander scripts

--- Assets/AutonomousAgent/Scripts/AutonomousAgent.cs
+++ Assets/AutonomousAgent/Scripts/AutonomousAgent.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AutonomousAgent : AiAgent
@@ -9,13 +10,26 @@ public class AutonomousAgent : AiAgent
     [SerializeField] Perception seekPerception;
     [SerializeField] Perception fleePerception;
 
+    [Header("Wander")]
+
+    [SerializeField] float wanderRadius = 1;
+
+    [SerializeField] float wanderDistance = 1;
+
+    [SerializeField] float wanderDisplacement = 1;
+
+
+
+    float wanderAngle = 0.0f;
+
 
 
     void Start()
 
     {
+        // random within circle degrees (random range 0.0f-360.0f) 
 
-
+        wanderAngle = Random.Range(0, 360);
 
     }
 
@@ -23,11 +37,13 @@ public class AutonomousAgent : AiAgent
 
     void Update()
     {
+        bool hasTarget = false;
         if (seekPerception != null)
         {
             var gameobjects = seekPerception.GetGameObjects();
             if (gameobjects.Length > 0)
             {
+                hasTarget = true;
                 Vector3 force = Seek(gameobjects[0]);
                 movement.ApplyForce(force);
             }
@@ -41,6 +57,7 @@ public class AutonomousAgent : AiAgent
             var gameobjects = fleePerception.GetGameObjects();
             if (gameobjects.Length > 0)
             {
+                hasTarget = true;
                 Vector3 force = Flee(gameobjects[0]);
                 movement.ApplyForce(force);
             }
@@ -48,6 +65,12 @@ public class AutonomousAgent : AiAgent
             {
                 Debug.DrawLine(transform.position, go.transform.position, Color.green);
             }
+        }
+
+        if (!hasTarget)
+        {
+            Vector3 force = Wander();
+            movement.ApplyForce(force);
         }
 
         transform.position = Utilities.Wrap(transform.position, new Vector3(-15, -15, -15), new Vector3(15, 15, 15));
@@ -67,6 +90,23 @@ public class AutonomousAgent : AiAgent
         Vector3 direction = transform.position - go.transform.position;
         Vector3 force = GetSteeringForce(direction);
         return force;
+    }
+
+    private Vector3 Wander()
+
+    {
+
+        
+
+        wanderAngle += Random.Range(-wanderDisplacement, wanderDisplacement);
+        Quaternion rotation = Quaternion.AngleAxis(wanderAngle, Vector3.up);
+        Vector3 pointOnCircle = rotation * (Vector3.forward * wanderRadius);
+        Vector3 circleCenter = movement.Velocity.normalized * wanderDistance;
+        Vector3 force = GetSteeringForce(circleCenter + pointOnCircle);
+        Debug.DrawLine(transform.position, transform.position + circleCenter, Color.blue);
+        Debug.DrawLine(transform.position, transform.position + circleCenter + pointOnCircle, Color.red);
+        return force;
+
     }
 
     Vector3 GetSteeringForce(Vector3 direction) {
